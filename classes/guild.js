bdd8b8f8b132533bdc853b8fcbb79dc0a4d33460
TheChickenKnight <81781990+TheChickenKnight@users.noreply.github.com>
2022-03-02 22:24:22 -0500

    guild class finished

--- classes/guild.js
+++ classes/guild.js
@@ -1,3 +1,31 @@
-class MyGuild {
- 
+import { client } from "../index.js";
+
+export class MyGuild {
+    constructor(obj) {
+        let keys;
+        if (obj) {
+            keys = Object.keys(obj);
+            if (keys.includes('obj'))
+                Object.assign(this, obj.obj);
+            else if (keys.includes('string'))
+                Object.assign(this, JSON.parse(obj.string));
+        }
+    }
+
+
+    async fromID(id) {
+        const guild = client.guilds.cache.get(id);
+        Object.assign(this, JSON.parse(await client.redis.HGET('guildSpec', id)) || {
+            prefix: 's!',
+            name: guild.name,
+            id: id,
+            new : true,
+            commands: 0
+        });
+        return this;
+    }
+
+    async set() {
+        await client.redis.HSET('guildSpec', this.id, JSON.stringify(this));
+    }
 }
