e70234a878aaf724515a2d8cb7353bcbe7f9035c
TheChickenKnight <81781990+TheChickenKnight@users.noreply.github.com>
2022-03-05 10:34:50 -0500

    welcome doesn't have to be async rn

--- welcome.js
+++ welcome.js
@@ -1,7 +1,7 @@
 import { MessageAttachment, MessageEmbed } from "discord.js";
 import { themedColor } from "./functions.js";
 
-export async function welcome(client, message) {
+export function welcome(client, message) {
     const welcome = new MessageAttachment().setFile('./welcome.png');
     message.reply({
         embeds: [
